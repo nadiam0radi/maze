c580e748f8fb648c02e9c83c2ab086d590a56749
nadia <moradi.nadiw@gmail.com>
2026-06-18 07:32:47 +0330

    Generating shapes

--- index.js
+++ index.js
@@ -1,28 +1,71 @@
-const { Engine , Render , Runner , World , Bodies , MouseConstraint , Mouse}= Matter
-const engine = Engine.create()
-const {world} =  engine
+const {
+  Engine,
+  Render,
+  Runner,
+  World,
+  Bodies,
+  MouseConstraint,
+  Mouse
+} = Matter;
+
+const width = 800;
+const height = 600;
+
+const engine = Engine.create();
+const { world } = engine;
+
 const render = Render.create({
-    element: document.body,
-    engine:engine,
-    Option:{
-        with:800,
-        height:600
-    }
-})
-Render.run(render)
-Runner.run(Runner.create(),engine)
+  element: document.body,
+  engine: engine,
+  options: {
+    wireframes: false,
+    width: width,
+    height: height
+  }
+});
 
-World.add(world , MouseConstraint.create(engine , {
-    mouse: Mouse.create(render.canvas)
-}))
+Render.run(render);
 
-//walls
+const runner = Runner.create();
+Runner.run(runner, engine);
+
+World.add(world, MouseConstraint.create(engine, {
+  mouse: Mouse.create(render.canvas)
+}));
+
+// walls
 const walls = [
-    Bodies.rectangle(400 , 0 , 800 , 40 , { isStatic : true}),
-    Bodies.rectangle(400 , 600 , 800 , 40 , {isStatic : true}),
-    Bodies.rectangle(0 , 300 , 40 , 600 , {isStatic : true}),
-    Bodies.rectangle(800 , 300 , 40 , 600 , {isStatic : true})
-]
-    
-World.add(world,walls)
-World.add(world, Bodies.rectangle(200, 200, 50, 50)) 
+  Bodies.rectangle(400, 0, 800, 40, { isStatic: true }),
+  Bodies.rectangle(400, 600, 800, 40, { isStatic: true }),
+  Bodies.rectangle(0, 300, 40, 600, { isStatic: true }),
+  Bodies.rectangle(800, 300, 40, 600, { isStatic: true })
+];
+
+World.add(world, walls);
+
+for (let i = 0; i < 50; i++) {
+  if (Math.random() > 0.5) {
+    World.add(
+      world,
+      Bodies.rectangle(
+        Math.random() * width,
+        Math.random() * height,
+        50,
+        50
+      )
+    );
+  } else {
+    World.add(
+      world,
+      Bodies.circle(
+        Math.random() * width,
+        Math.random() * height,
+        35 , {
+            render: {
+                fillStyle: 'red'
+            }
+        }
+      )
+    );
+  }
+}
