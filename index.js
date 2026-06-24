b4a6a7f46e76ee48e4dd0cb7a5eefaee9a86cb98
nadia <moradi.nadiw@gmail.com>
2026-06-24 08:51:12 +0330

    Drawing Segments

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const cells = 3;
 const width = 600;
 const height = 600;
 
+const unitLength = length / cells
+
 const engine = Engine.create();
 const { world } = engine;
 const render = Render.create({
@@ -104,3 +106,21 @@ for(let neighbor of neighbors){
 };
 
 stepThroughCell(startRow, startColumn);
+
+horizontals.forEach((row, rowIndex) =>{
+  row.forEach((open, columnIndex) =>{
+    if (open){
+      return
+    }
+    const wall = Bodies.rectangle(
+      columnIndex * unitLength + unitLength /2,
+      rowIndex * unitLength + unitLength,
+      unitLength,
+      10,
+      {
+        isStatic: true
+      }
+    )
+    World.add(world, wall)
+  })
+})
